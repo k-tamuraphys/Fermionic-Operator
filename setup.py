303bae5dccef29d762788d4f2915029ffce5a01c
Kensuke Tamura <47786196+k-tamuraphys@users.noreply.github.com>
2022-06-01 00:19:32 +0900

    minor update

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ with open("requirements.txt") as f:
 
 setuptools.setup(
     name="fermionic-operator",
-    version="0.1.0",
+    version="0.0.1",
     description="Fermionic Operator",
     author="Kensuke Tamura",
     install_requires=REQUIREMENTS,
+    packages=setuptools.find_packages(),
 )
